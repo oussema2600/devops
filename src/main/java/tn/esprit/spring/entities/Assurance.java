28bcbdfa76ea54c5bb77e9c3efdc63ef8b21a30d
oussama hammami <hammami.oussama@esprit.tn>
2023-10-16 19:06:46 +0100

    ajout commentaire

---
+++ src/main/java/tn/esprit/spring/entities/Assurance.java
@@ -0,0 +1,32 @@
+package tn.esprit.spring.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Assurance implements Serializable{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idAssurance;
+
+	private String designation;
+	private float montant;
+	@ManyToOne(cascade=CascadeType.ALL)
+	private Contrat contrat;
+
+	@ManyToOne
+	private Beneficiaire beneficiaire;
+	
+
+}
---
+++ src/main/java/tn/esprit/spring/repositories/IBeneficiaireRepository.java
@@ -0,0 +1,31 @@
+package tn.esprit.spring.repositories;
+
+import java.util.Set;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import tn.esprit.spring.entities.Beneficiaire;
+import tn.esprit.spring.entities.TypeContrat;
+
+@Repository
+public interface IBeneficiaireRepository extends CrudRepository<Beneficiaire, Integer>{
+
+	Beneficiaire getByCin(int cinBenef);
+	
+	@Query("Select b From Beneficiaire b join b.assurances ass join " +
+			"ass.contrat c where c.type = :tc")
+	Set<Beneficiaire> getByAssuranceType(@Param("tc") TypeContrat tc);
+
+
+	@Query("Select beneficiaire From Beneficiaire beneficiaire " +
+			"inner join Assurance assurance " +
+			"inner join Contrat contrat " +
+			"on beneficiaire.idBeneficiaire = assurance.beneficiaire.idBeneficiaire " +
+			"and assurance.contrat.idContrat = contrat.idContrat and contrat.type = :tc")
+	Set<Beneficiaire> getByAssuranceTypee(@Param("tc") TypeContrat tc);
+
+
+}
